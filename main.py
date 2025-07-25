6fba08adb2dfabd86efcffa613b33304ab2bb895
maneesh <maneeshreddyenugala@gmail.com>
2025-07-25 23:32:18 +0530

    Fix search logic to return URLs containing the keyword

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class WebCrawler:
     def search(self, keyword):
         results = []
         for url, text in self.index.items():
-            if keyword.lower() not in text.lower():
+            if keyword.lower() in text.lower():
                 results.append(url)
         return results
 
